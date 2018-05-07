3e76acd357244208921dfec28366743aff52c7a7
Alexander Emelin <frvzmb@gmail.com>
2018-05-08 00:06:35 +0300

    add example

---
+++ Centrifuge-iOS/Centrifuge-iOS/ViewController.swift
@@ -0,0 +1,127 @@
+//
+//  ViewController.swift
+//  Centrifuge-iOS
+//
+//  Created by Alexander Emelin on 07/05/2018.
+//  Copyright © 2018 Alexander Emelin. All rights reserved.
+//
+
+import UIKit
+import Centrifuge
+
+class ConnectHandler : NSObject, CentrifugeConnectHandlerProtocol {
+    var l: UILabel!
+    
+    func setLabel(l: UILabel!) {
+        self.l = l
+    }
+    
+    func onConnect(_ p0: CentrifugeClient!, p1: CentrifugeConnectEvent!) {
+        DispatchQueue.main.async{
+            self.l.text = "Connected with id " + p1.clientID();
+        }
+    }
+}
+
+class DisconnectHandler : NSObject, CentrifugeDisconnectHandlerProtocol {
+    var l: UILabel!
+    
+    func setLabel(l: UILabel!) {
+        self.l = l
+    }
+    
+    func onDisconnect(_ p0: CentrifugeClient!, p1: CentrifugeDisconnectEvent!) {
+        DispatchQueue.main.async{
+            self.l.text = "Disconnected: " + p1.reason();
+        }
+    }
+}
+
+class PublishHandler : NSObject, CentrifugePublishHandlerProtocol {
+    var l: UILabel!
+    
+    func setLabel(l: UILabel!) {
+        self.l = l
+    }
+    
+    func onPublish(_ p0: CentrifugeSubscription!, p1: CentrifugePublishEvent!) {
+        DispatchQueue.main.async{
+            let data = p1.data()
+            if let string = String(data: data!, encoding: .utf8) {
+                self.l.text = string
+            } else {
+                self.l.text = "error decoding Publication bytes"
+            }
+        }
+    }
+}
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        label.text = "Connecting..."
+        
+        DispatchQueue.main.async{
+//            let creds = CentrifugeNewCredentials(...)
+            
+            let eventHandler = CentrifugeNewEventHub()
+            let connectHandler = ConnectHandler()
+            connectHandler.setLabel(l: self.label)
+            let disconnectHandler = DisconnectHandler()
+            disconnectHandler.setLabel(l: self.label)
+            
+            eventHandler?.onConnect(connectHandler)
+            eventHandler?.onDisconnect(disconnectHandler)
+            
+            let url = "ws://localhost:8000/connection/websocket"
+            let client = CentrifugeNew(url, eventHandler, CentrifugeDefaultConfig())
+//            client?.setCredentials(creds)
+            
+            do {
+                try client?.connect()
+            } catch {
+                self.label.text = "Error on connect..."
+                return
+            }
+            
+            self.label.text = "Connected"
+            
+            let subEventHandler = CentrifugeNewSubscriptionEventHub()
+            let publishHandler = PublishHandler()
+            publishHandler.setLabel(l: self.label)
+            subEventHandler?.onPublish(publishHandler)
+            
+            var sub: CentrifugeSubscription!
+            do {
+                sub = try client?.subscribe("chat:index", events: subEventHandler)
+            } catch {
+                DispatchQueue.main.async{
+                    self.label.text = "Subscribe error"
+                }
+                return
+            }
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3), execute: {
+                do {
+                    let data = "{\"input\": \"hey there from iOS example!\"}".data(using: .utf8)
+                    try sub?.publish(data)
+                } catch {
+                    self.label.text = "Publish error"
+                    return
+                }
+            })
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ readme.md
@@ -0,0 +1,9 @@
+Simple example of using [centrifuge-mobile](https://github.com/centrifugal/centrifuge-mobile) library for iOS development.
+
+You need to download library build for iOS from [releases page](https://github.com/centrifugal/centrifuge-mobile/releases).
+
+After unpacking drag `Centrifuge.framework` to your application workspace and you are ready to use library.
+
+Look at example in this repo to get basic understanding of how to use library.
+
+Don't forget to run Centrifuge-based server (or Centrifugo). And also change connection address in example to yours if needed.
